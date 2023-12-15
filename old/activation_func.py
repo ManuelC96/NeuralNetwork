869e7c26baa08f10d365dbf22068c58c1ff984c4
ManuelC96 <manuelchiocchetta@gmail.com>
2023-12-15 10:49:25 +0100

    commit

--- old/activation_func.py
+++ old/activation_func.py
@@ -1,8 +1,9 @@
-from activation import Activation
+from activation_layer import Activation
 import numpy as np
-# 
+#collection of various activation functions 
 class Tanh(Activation):
     def __init__(self):
         tanh = lambda x: np.tanh(x)
         tanh_prime = lambda x: 1 - np.tanh(x)
         super().__init__(tanh, tanh_prime)
+
